7352ae7d152b341ef57517407e7533d6651c2612
Jediael Mendoza <mend0214@algonquinlive.com>
2026-03-15 14:08:23 -0400

    fix terraform formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "azurerm" {
-features {}
-subscription_id = var.subscription_id
+  features {}
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
-name     = var.resource_group_name
-location = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
